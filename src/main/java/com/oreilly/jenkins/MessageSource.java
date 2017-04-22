0e163541a90e1f7ecd220eaf5e4ac874daaf2b9c
Kevin Bowersox <kmb385@gmail.com>
2017-04-22 09:50:50 -0400

    adding

---
+++ src/main/java/com/oreilly/jenkins/MessageSource.java
@@ -0,0 +1,11 @@
+package com.oreilly.jenkins;
+
+public class MessageSource {
+
+	private String message = "Hello from the message source.";
+
+	public String getMessage() {
+		return message;
+	}
+
+}
