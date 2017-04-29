2299c9a110b0f17dae842b863297febdf891d4b0
Kevin Bowersox <kmb385@gmail.com>
2017-04-29 11:42:55 -0400

    adding tests

---
+++ src/test/java/com/oreilly/jenkins/MessageSourceTest.java
@@ -0,0 +1,13 @@
+package com.oreilly.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageSourceTest{
+
+	@Test
+	public void test(){
+		fail("Not implemented");
+	}
+}
