7dee25ff95d7903a65a03f8fcf724a097ce40c66
Kevin Bowersox <kmb385@gmail.com>
2017-04-30 07:20:29 -0400

    Updated

--- src/main/java/com/oreilly/jenkins/MessageSource.java
+++ src/main/java/com/oreilly/jenkins/MessageSource.java
@@ -5,6 +5,9 @@ public class MessageSource {
 	private String message = "Hi from the message source.";
 
 	public String getMessage() {
+		if(message == "Hello"){
+			System.out.println("something");
+		}
 		return message;
 	}
 
